858a30c167240b41bc57e523815a15002adf05f2
muskankapoor <muskankapoor016@gmail.com>
2021-03-17 15:14:41 -0400

    update

--- src/hooks/useActionRemoveRow.js
+++ src/hooks/useActionRemoveRow.js
@@ -4,7 +4,7 @@ import { GridCell } from "../util/generate_grid_nodes";
 const useActionRemoveRow = (gridNodes, setGridNodes) => {
   const handleSelect = () => {
 
-    // while length of gridNodes.length not zero the gridNodes pop
+    // if length of gridNodes.length is not zero --> gridNodes pop
     // if it is zero (or no more gridNodes left), this also make sure it doesn't give any out of bound error
     // for the user it makes no difference. can press button as many times as uder desires
     if (!gridNodes.length == 0){ 
