46bce077fbb8aa468404dc2ec830917ff960a803
muskankapoor <muskankapoor016@gmail.com>
2021-03-18 17:44:51 -0400

    fixed warning

--- src/hooks/useActionRemoveRow.js
+++ src/hooks/useActionRemoveRow.js
@@ -9,10 +9,9 @@ const useActionRemoveRow = (gridNodes, setGridNodes) => {
     // if length of gridNodes.length is not zero --> gridNodes pop
     // if it is zero (or no more gridNodes left), this also make sure it doesn't give any out of bound error
     // for the user it makes no difference. can press button as many times as uder desires
-    if (!gridNodesCopy.length == 0){ 
+   if (gridNodesCopy.length !== 0){ 
       gridNodes.pop();
     }
-    
     setGridNodes([...gridNodes]);
 };
 
