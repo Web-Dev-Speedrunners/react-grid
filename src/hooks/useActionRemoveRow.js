c0de210c639614660f6f49bca053e9823ff90cce
muskankapoor <muskankapoor016@gmail.com>
2021-03-18 15:11:10 -0400

    update remove Row with an array that copies gridNodes

--- src/hooks/useActionRemoveRow.js
+++ src/hooks/useActionRemoveRow.js
@@ -4,10 +4,12 @@
 const useActionRemoveRow = (gridNodes, setGridNodes) => {
   const handleSelect = () => {
 
+    // reates a new, shallow-copied Array instance from an array-like object (gridNodes).
+    let gridNodesCopy = Array.from(gridNodes);
     // if length of gridNodes.length is not zero --> gridNodes pop
     // if it is zero (or no more gridNodes left), this also make sure it doesn't give any out of bound error
     // for the user it makes no difference. can press button as many times as uder desires
-    if (!gridNodes.length == 0){ 
+    if (!gridNodesCopy.length == 0){ 
       gridNodes.pop();
     }
     
